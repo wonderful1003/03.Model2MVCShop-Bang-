ef95e0d3adadca7ccc9e52df11c0eccc48e1ac45
bang <bang@Bang>
2017-08-07 00:15:16 +0900

    업데이트및 서비스 수정

--- 03.Model2MVCShop(stu)/src/com/model2/mvc/service/purchase/impl/PurchaseServiceImpl.java
+++ 03.Model2MVCShop(stu)/src/com/model2/mvc/service/purchase/impl/PurchaseServiceImpl.java
@@ -4,24 +4,29 @@ import java.util.HashMap;
 
 import com.model2.mvc.common.Search;
 import com.model2.mvc.service.domain.Purchase;
+import com.model2.mvc.service.product.impl.ProductServiceImpl;
 import com.model2.mvc.service.purchase.PurchaseService;
+import com.model2.mvc.service.purchase.dao.PurchaseDAO;
 
 public class PurchaseServiceImpl implements PurchaseService{
 	
+	PurchaseDAO purchaseDAO = null;
+	
 	public PurchaseServiceImpl() {
 		// TODO Auto-generated constructor stub
+		purchaseDAO = new PurchaseDAO();
 	}
 
 	@Override
 	public Purchase getPurchase(int tranNo) throws Exception {
 		// TODO Auto-generated method stub
-		return null;
+		return purchaseDAO.findPurchase(tranNo);
 	}
 
 	@Override
 	public HashMap<String, Object> getPurchaseList(Search search, String buyerId) throws Exception {
 		// TODO Auto-generated method stub
-		return null;
+		return purchaseDAO.getPurchaseList(search, buyerId);
 	}
 
 	@Override
@@ -31,21 +36,29 @@ public class PurchaseServiceImpl implements PurchaseService{
 	}
 
 	@Override
-	public Purchase addPurchase(Purchase purchase) throws Exception {
+	public void addPurchase(Purchase purchase) throws Exception {
 		// TODO Auto-generated method stub
-		return null;
+		purchaseDAO.insertPurchase(purchase);
 	}
 
 	@Override
 	public void updatePurchase(Purchase purchase) throws Exception {
 		// TODO Auto-generated method stub
-	
+		purchaseDAO.updatePurchase(purchase);
+	}
+
+	@Override
+	public Purchase getPurchaseByProd(int prodNo) throws Exception {
+		// TODO Auto-generated method stub
+		Purchase purchase = new Purchase();
+		purchase.setPurchaseProd((new ProductServiceImpl().getProduct(prodNo)));
+		return purchase;
 	}
 
 	@Override
 	public void updateTranCode(Purchase purchase) throws Exception {
 		// TODO Auto-generated method stub
-		
+		purchaseDAO.updateTranCode(purchase);
 	}
 
 
--- 03.Model2MVCShop(stu)/src/com/model2/mvc/view/purchase/UpdateTranCodeAction.java
+++ 03.Model2MVCShop(stu)/src/com/model2/mvc/view/purchase/UpdateTranCodeAction.java
@@ -4,6 +4,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.model2.mvc.framework.Action;
+import com.model2.mvc.service.domain.Purchase;
+import com.model2.mvc.service.purchase.PurchaseService;
+import com.model2.mvc.service.purchase.impl.PurchaseServiceImpl;
 
 public class UpdateTranCodeAction extends Action {
 
@@ -15,9 +18,13 @@ public class UpdateTranCodeAction extends Action {
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		// TODO Auto-generated method stub
 		
+		PurchaseService service = new PurchaseServiceImpl();
+		Purchase purchase = service.getPurchase(Integer.parseInt(request.getParameter("tranNo")));
+		purchase.setTranCode(request.getParameter("tranCode"));
+		service.updateTranCode(purchase);
 		
 		
-		return null;
+		return "forward:/listPurchase.do";
 	}
 
 }
